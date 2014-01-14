5794428e63b824e536e1f340f07fc8d0fe4f0a0f
Stephen Compton <sccompton@me.com>
2014-01-14 14:25:54 -0600

    Initial Commit

---
+++ MythicalCreatureManagementSystem/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MythicalCreatureManagementSystem
+//
+//  Created by Stephen Compton on 1/14/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
